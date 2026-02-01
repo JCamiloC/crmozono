9373c6a71f588ea8de4628b077dc9da657f71d3e
JCamiloC <cerinzajuanc@gmail.com>
2026-02-01 18:13:41 -0500

    fIRST dEPLOY

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath,
+  assetPrefix: basePath || undefined,
 };
 
 export default nextConfig;
